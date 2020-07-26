d3bc33bb2714325c0ac8a2943c2d945a8e05417c
Benjamin Jin <benjin@ethz.ch>
2020-07-26 11:45:50 +0200

    Got the list with idx triples and new reference timestamps

--- preprocessing/preprocessing/data_preprocesser.py
+++ preprocessing/preprocessing/data_preprocesser.py
@@ -1,24 +1,66 @@
 import os
-from utils.utils import get_camera_timestamps
+from utils.utils import get_camera_timestamps, timestamps_within_interval
+import numpy as np
 
 
 class DataPreprocesser:
     def __init__(self, cfg):
         self.data_folder_path = cfg.data_folder_path
 
+        # Constants
+        self.MIN_DTIMESTAMP_THRESHOLD = 0.01
+
     def match_images(self):
         # Read in the timestamp files
-        timestamp_arrays_dict = get_camera_timestamps()
+        timestamp_arrays_dict = get_camera_timestamps(self.data_folder_path)
 
         # For every forward camera image timestamp, find the closest
         # timestamps from the left and right camera images
-        for idx, forward_camera_timestamp in enumerate(
+        idx_triples = []
+        new_reference_timestamps = []
+
+        for forward_camera_timestamp_idx, forward_camera_timestamp in enumerate(
                 timestamp_arrays_dict["forward_camera"]):
 
-            left_camera_timestamp_idx = np.argmin(
-                np.abs(timestamp_arrays_dict["left_camera"] -
-                       forward_camera_timestamp))
+            left_camera_dtimestamps = np.abs(
+                timestamp_arrays_dict["left_camera"] -
+                forward_camera_timestamp)
+            left_camera_min_dtimestamp_idx = np.argmin(left_camera_dtimestamps)
+            left_camera_min_dtimestamp = left_camera_dtimestamps[
+                left_camera_min_dtimestamp_idx]
 
-            right_camera_timestamp_idx = np.argmin(
-                np.abs(timestamp_arrays_dict["right_camera"] -
-                       forward_camera_timestamp))
+            right_camera_dtimestamps = np.abs(
+                timestamp_arrays_dict["right_camera"] -
+                forward_camera_timestamp)
+            right_camera_min_dtimestamp_idx = np.argmin(
+                right_camera_dtimestamps)
+            right_camera_min_dtimestamp = right_camera_dtimestamps[
+                right_camera_min_dtimestamp_idx]
+
+            if left_camera_min_dtimestamp < self.MIN_DTIMESTAMP_THRESHOLD and right_camera_min_dtimestamp < self.MIN_DTIMESTAMP_THRESHOLD:
+
+                left_camera_timestamp = timestamp_arrays_dict["left_camera"][
+                    left_camera_min_dtimestamp_idx]
+                right_camera_timestamp = timestamp_arrays_dict["right_camera"][
+                    right_camera_min_dtimestamp_idx]
+
+                if timestamps_within_interval(
+                        self.MIN_DTIMESTAMP_THRESHOLD,
+                    (forward_camera_timestamp, left_camera_timestamp,
+                     right_camera_timestamp)):
+
+                    # Add the indices of the valid triple to a list
+                    current_idx_triple = [
+                        forward_camera_timestamp_idx,
+                        left_camera_min_dtimestamp_idx,
+                        right_camera_min_dtimestamp_idx
+                    ]
+                    idx_triples.append(current_idx_triple)
+
+                    new_reference_timestamps.append(
+                        np.mean([
+                            forward_camera_timestamp, left_camera_timestamp,
+                            right_camera_timestamp
+                        ]))
+
+        print("done")
--- preprocessing/preprocessing/utils/utils.py
+++ preprocessing/preprocessing/utils/utils.py
@@ -1,11 +1,15 @@
-def get_camera_timestamps():
+import numpy as np
+import os
+
+
+def get_camera_timestamps(data_folder_path):
     timestamp_filepaths_dict = {
         "forward_camera":
-        os.path.join(self.data_folder_path, "forward_camera/timestamps.txt"),
+        os.path.join(data_folder_path, "forward_camera/timestamps.txt"),
         "left_camera":
-        os.path.join(self.data_folder_path, "left_camera/timestamps.txt"),
+        os.path.join(data_folder_path, "left_camera/timestamps.txt"),
         "right_camera":
-        os.path.join(self.data_folder_path, "right_camera/timestamps.txt")
+        os.path.join(data_folder_path, "right_camera/timestamps.txt")
     }
 
     return read_timestamps(timestamp_filepaths_dict)
@@ -23,3 +27,9 @@ def read_timestamps(timestamp_filepaths_dict):
             timestamp_arrays_dict[key] = np.array(timestamp_arrays_dict[key],
                                                   dtype=np.float)
     return timestamp_arrays_dict
+
+
+def timestamps_within_interval(interval, timestamps):
+    min_timestamp = np.min(np.array(timestamps))
+    max_timestamp = np.max(np.array(timestamps))
+    return max_timestamp - min_timestamp < interval
