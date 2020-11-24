aa122cf1d733ccb31e41a633b3058cd24000f11f
Benjamin Jin <benjin@ethz.ch>
2020-11-24 20:02:06 +0100

    Processing of all GT from right, left and forward camera + visualization

--- evaluation/eval_3d_cone_positions.py
+++ evaluation/eval_3d_cone_positions.py
@@ -4,8 +4,8 @@ import numpy as np
 import sys
 import cv2
 import open3d as o3d
-
-# Would be good to know which data folders carter is using as test data atm
+import yaml
+import matplotlib.pyplot as plt
 
 
 def command_line_parser():
@@ -37,7 +37,7 @@ def command_line_parser():
 
     parser.add_argument('-m',
                         '--mode',
-                        default='vis',
+                        default='metrics',
                         type=str,
                         choices=['vis, metrics'],
                         help='Specify what the program should do')
@@ -50,7 +50,7 @@ def command_line_parser():
 def match_cone_arrays(cfg):
     # Get ground truth cone arrays in mrh_lidar frame
     # Use the GT cone arrays from the left and right camera frames
-    gt_cone_arrays_dict = {"left": {}, "right": {}}
+    gt_cone_arrays_dict = {"left": {}, "right": {}, "forward": {}}
     indices = []
 
     # Pathing
@@ -58,18 +58,38 @@ def match_cone_arrays(cfg):
     static_transformations_folder = os.path.join(data_folder, "..", "..",
                                                  "static_transformations")
 
-    for camera in gt_cone_arrays_dict.keys():
-        # Get static transformation
-        T = np.eye(4)
+    # Get static transformation: mrh to ego
+    T_mrh_ego = np.eye(4)
 
-        T_file = os.path.join(static_transformations_folder,
-                              "extrinsics_mrh_" + camera + ".yaml")
-        transformation_file = cv2.FileStorage(T_file, cv2.FILE_STORAGE_READ)
-        R = transformation_file.getNode("R_mtx").mat()
-        t = transformation_file.getNode("t_mtx").mat()
+    T_mrh_ego_file = os.path.join(static_transformations_folder,
+                                  "static_transformations.yaml")
+    with open(T_mrh_ego_file, "r") as f:
+        transformations = yaml.load(f, Loader=yaml.FullLoader)
+    for key in transformations.keys():
+        rot = transformations[key]['mrh_lidar_to_egomotion']['rotation']
+        trans = transformations[key]['mrh_lidar_to_egomotion']['translation']
+    rotv = np.array([rot['roll'], rot['pitch'], rot['yaw']])
+    R_mrh_ego, _ = cv2.Rodrigues(rotv)
+    t_mrh_ego = np.array([trans['x'], trans['y'], trans['z']])
+    T_mrh_ego[:3, :3], T_mrh_ego[:3, 3] = R_mrh_ego, t_mrh_ego.reshape(-1)
+
+    for camera in gt_cone_arrays_dict.keys():
+        # Get static transformation: camera to mrh
+        T_mrh_camera = np.eye(4)
+
+        T_mrh_camera_file = os.path.join(static_transformations_folder,
+                                         "extrinsics_mrh_" + camera + ".yaml")
+        transformation_file = cv2.FileStorage(T_mrh_camera_file,
+                                              cv2.FILE_STORAGE_READ)
+        R_mrh_camera = transformation_file.getNode("R_mtx").mat()
+        t_mrh_camera = transformation_file.getNode("t_mtx").mat()
         transformation_file.release()
-        T[:3, :3], T[:3, 3] = R, t.reshape(-1)
-        T = np.linalg.inv(T)
+        T_mrh_camera[:3, :
+                     3], T_mrh_camera[:3,
+                                      3] = R_mrh_camera, t_mrh_camera.reshape(
+                                          -1)
+        T_camera_mrh = np.linalg.inv(T_mrh_camera)
+        T_camera_ego = T_mrh_ego @ T_camera_mrh
 
         # Get ground truth cone arrays in mrh frame
         gt_cone_arrays = []
@@ -96,7 +116,7 @@ def match_cone_arrays(cfg):
             cone_positions = gt_cone_array[:, 1:]
             h_cone_positions = np.hstack(
                 (cone_positions, np.ones((cone_positions.shape[0], 1))))
-            h_cone_positions = T @ h_cone_positions.T
+            h_cone_positions = T_camera_ego @ h_cone_positions.T
             gt_cone_array = np.hstack(
                 (cone_types.reshape(-1, 1), h_cone_positions.T[:, :3]))
             gt_cone_arrays_dict[camera][
@@ -104,9 +124,9 @@ def match_cone_arrays(cfg):
 
         indices.extend(list(gt_cone_arrays_dict[camera].keys()))
 
-    # Filter indices when there were gt cones for the left and right camera
+    # Get all indices for which we have gt cone arrays
     u, c = np.unique(np.array(indices), return_counts=True)
-    indices = u[c > 1]
+    indices = u[c > 0]
 
     # Get lidar pipeline cone arrays
     lidar_cone_arrays = []
@@ -121,27 +141,28 @@ def match_cone_arrays(cfg):
     for lidar_cone_array_file in lidar_cone_array_files:
         lidar_cone_array_file = os.path.join(lidar_cone_array_folder,
                                              lidar_cone_array_file)
-        lidar_cone_arrays.append(
-            np.fromfile(lidar_cone_array_file).reshape(-1, 3))
+        lidar_cone_array = np.fromfile(lidar_cone_array_file).reshape(-1, 3)
+        lidar_cone_arrays.append(lidar_cone_array)
 
     # Filter comparable data
-    cone_array_types = ["gt_left", "gt_right", "lidar"]
-    cone_arrays_dict = {}
-    for cone_array_type in cone_array_types:
-        cone_arrays_dict[cone_array_type] = []
-        if cone_array_type == "gt_left":
-            for idx in indices:
-                cone_arrays_dict[cone_array_type].append(
-                    gt_cone_arrays_dict["left"][idx])
-        elif cone_array_type == "gt_right":
-            for idx in indices:
-                cone_arrays_dict[cone_array_type].append(
-                    gt_cone_arrays_dict["right"][idx])
-        elif cone_array_type == "lidar":
-            for idx in indices:
-                cone_arrays_dict[cone_array_type].append(
-                    lidar_cone_arrays[idx])
+    cone_arrays_dict = {"lidar": [], "gt": []}
+    gt_indices = []
+
+    for idx in indices:
+        lidar_cone_array = lidar_cone_arrays[idx]
+        if lidar_cone_array.shape[0] != 0:
+            cone_arrays_dict["lidar"].append(lidar_cone_array)
+            gt_indices.append(idx)
+
+    for idx in gt_indices:
+        if idx in gt_cone_arrays_dict["forward"]:
+            cone_arrays_dict["gt"].append(gt_cone_arrays_dict["forward"][idx])
+        elif idx in gt_cone_arrays_dict["right"]:
+            cone_arrays_dict["gt"].append(gt_cone_arrays_dict["right"][idx])
+        elif idx in gt_cone_arrays_dict["left"]:
+            cone_arrays_dict["gt"].append(gt_cone_arrays_dict["left"][idx])
         else:
+            print("There exists no ground truth cone array with this index!")
             sys.exit()
 
     return cone_arrays_dict
@@ -151,32 +172,75 @@ def visualize_cone_arrays(cfg):
     cone_arrays_dict = match_cone_arrays(cfg)
     num_cone_arrays = len(cone_arrays_dict["lidar"])
 
-    pcd_lidar = o3d.geometry.PointCloud()
-    pcd_lidar.points = o3d.utility.Vector3dVector(cone_arrays_dict["lidar"][0])
-    pcd_lidar.paint_uniform_color([1.0, 0.1, 0.1])
+    for cone_array_idx in range(num_cone_arrays):
+        pcd_lidar = o3d.geometry.PointCloud()
+        pcd_lidar.points = o3d.utility.Vector3dVector(
+            cone_arrays_dict["lidar"][cone_array_idx])
+        pcd_lidar.paint_uniform_color([1.0, 0.1, 0.1])
 
-    pcd_gt_left = o3d.geometry.PointCloud()
-    pcd_gt_left.points = o3d.utility.Vector3dVector(
-        cone_arrays_dict["gt_left"][0][:, 1:])
-    pcd_gt_left.paint_uniform_color([0.1, 0.9, 0.1])
+        pcd_gt = o3d.geometry.PointCloud()
+        # Project the gt cones onto the xy plane
+        gt_cone_array_projected = np.hstack(
+            (cone_arrays_dict["gt"][cone_array_idx][:, 1:3],
+             np.zeros((cone_arrays_dict["gt"][cone_array_idx].shape[0], 1))))
+        # Artificially make a point to have a z!=0 to counter the open3d visualization bug
+        gt_cone_array_projected[-1, -1] = 1
+        pcd_gt.points = o3d.utility.Vector3dVector(gt_cone_array_projected)
+        pcd_gt.paint_uniform_color([0.1, 0.9, 0.1])
 
-    pcd_gt_right = o3d.geometry.PointCloud()
-    pcd_gt_right.points = o3d.utility.Vector3dVector(
-        cone_arrays_dict["gt_right"][0][:, 1:])
-    pcd_gt_right.paint_uniform_color([0.1, 0.1, 0.9])
+        # Coordinate Frame
+        mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=0.6, origin=[0, 0, 0])
 
-    # Coordinate Frame
-    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
-        size=0.6, origin=[0, 0, 0])
+        o3d.visualization.draw_geometries([pcd_lidar, pcd_gt, mesh_frame])
 
-    o3d.visualization.draw_geometries(
-        [pcd_lidar, pcd_gt_left, pcd_gt_right, mesh_frame])
-
-    print()
+    print("Done")
 
 
 def calculate_metrics(cfg):
-    pass
+    cone_arrays_dict = match_cone_arrays(cfg)
+    num_cone_arrays = len(cone_arrays_dict["lidar"])
+
+    MAX_RANGE = 80
+    D_RANGE = 2
+    MAX_DIST = 0.5
+
+    depth_metric = np.zeros((2, MAX_RANGE // D_RANGE))
+
+    for idx in range(num_cone_arrays):
+        gt_cone_array = o3d.geometry.PointCloud()
+        gt_cone_array.points = o3d.utility.Vector3dVector(
+            cone_arrays_dict["gt"][idx][:, 1:])
+        gt_cone_array_tree = o3d.geometry.KDTreeFlann(gt_cone_array)
+
+        for lidar_cone in cone_arrays_dict["lidar"][idx]:
+
+            [k, idx_nn,
+             _] = gt_cone_array_tree.search_knn_vector_3d(lidar_cone, 1)
+            [k_, idx_ball, _] = gt_cone_array_tree.search_radius_vector_3d(
+                lidar_cone, MAX_DIST)
+
+            if idx_nn[0] in idx_ball:
+                gt_cone = cone_arrays_dict["gt"][idx][idx_nn[0], 1:]
+                dist_gt_origin = np.clip(int(np.linalg.norm(gt_cone) / 2), 0,
+                                         39)
+                dist_gt_lidar = np.linalg.norm(gt_cone - lidar_cone)
+
+                depth_metric[0, dist_gt_origin] += 1
+                depth_metric[1, dist_gt_origin] += dist_gt_lidar
+
+    for idx in range(depth_metric.shape[1]):
+        if depth_metric[0, idx] != 0:
+            depth_metric[1, idx] = depth_metric[1, idx] / depth_metric[0, idx]
+
+    fig = plt.figure()
+    ax = fig.add_axes([0, 0, 1, 1])
+    dist_ranges = np.arange(0, 80, 2).tolist()
+    ax.bar([str(dist_range) for dist_range in dist_ranges],
+           depth_metric[1, :].tolist())
+    plt.show()
+
+    print("Done")
 
 
 def main():
